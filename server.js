e48a4ddda966f593f7e007f808a2896312e596bd
lander302 <lander304@caledonian.ac.uk>
2024-12-12 12:48:27 +0000

    syntax again

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var startTime;
 var host;
 
 var handleRequest = function(request, response) {
-  response.setHeader('Content-Type', 'text/plain)
+  response.setHeader('Content-Type', 'text/plain');
   response.writeHead(200);
   response.write("DevOps Coursework 2! | Running on: ");
   response.write(host);
